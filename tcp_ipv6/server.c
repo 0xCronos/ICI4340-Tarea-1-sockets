b54265d6950a27b628c06b0a883e5395e1f99587
Diego Muñoz <56097041+0xCronos@users.noreply.github.com>
2021-11-12 16:12:35 -0300

    Fix mispelled word

--- tcp_ipv6/server.c
+++ tcp_ipv6/server.c
@@ -25,7 +25,7 @@ int main(int argc, char const *argv[]) {
   server.sin6_port = htons(port);
   len = sizeof(server);
   if (bind(serverFd, (struct sockaddr *)&server, len) < 0) {
-    perror("Cannot bind sokcet");
+    perror("Cannot bind socket");
     exit(2);
   }
   if (listen(serverFd, 10) < 0) {
